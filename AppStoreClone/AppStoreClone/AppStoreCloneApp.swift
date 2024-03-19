a422fdf7b2679dc0b65578f7926cad88f8e67c40
jsa0224 <voidness45@gmail.com>
2024-03-19 15:08:14 +0900

    feat: 프로젝트 생성

---
+++ AppStoreClone/AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by 정선아 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
