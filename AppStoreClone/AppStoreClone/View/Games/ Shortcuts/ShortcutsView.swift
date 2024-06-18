496b64dc39133ad7a4f9f34e5a890ba6e86ece23
jsa0224 <voidness45@gmail.com>
2024-06-18 21:23:03 +0900

    feat: GamesView - ShortcutsView 적용

---
+++ AppStoreClone/AppStoreClone/View/Games/ Shortcuts/ShortcutsView.swift
@@ -0,0 +1,18 @@
+//
+//  ShortcutsView.swift
+//  AppStoreClone
+//
+//  Created by 정선아 on 6/18/24.
+//
+
+import SwiftUI
+
+struct ShortcutsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShortcutsView()
+}
